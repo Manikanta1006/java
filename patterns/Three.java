5b883b6b7d84e9ebc74c20edd2a4deab908e38b7
MANIKANTA <135311230+Manikanta1006@users.noreply.github.com>
2024-07-22 17:01:31 +0530

    Add files via upload

---
+++ patterns/Three.java
@@ -0,0 +1,10 @@
+public class Three {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){             //two programmes code change i and j
+                System.out.print(i);
+            }                           
+            System.out.println();
+        }
+    }
+}
